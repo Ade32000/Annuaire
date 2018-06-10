2dacc34f149c941326ea39f41fa96a94af8de436
Adeline <adeline.contact@gmail.com>
2018-06-10 15:54:23 +0200

    suite projet

--- annuaire.php
+++ annuaire.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,15 +8,20 @@
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title></title>
+	<title>Mon annuaire</title>
+	<nav class="menu">
+		<ul>
+			<li><a href="annuaire.php">Accueil</a></li>
+			<li><a href="formulaire.php">Ajouter</a></li>
+			<li><a href="#recherche">Rechercher</a></li>
+		</ul>
+	</nav>
 </head>
 <body>
+	 
 
-	
-	<nav></nav>
-
-
-<?php 
+<!-- --------------------------------------------v  connexion bdd et requêtes ici  v--------------------------------------------------- -->
+<?php
 try{
 	$bdd = new PDO('mysql:host=localhost;dbname=Mon_annuaire;charset=utf8', 'annie', '12345678');
 }
@@ -25,15 +32,31 @@ catch (Exception $e)
 }
 
 
-
 $reponse = $bdd->query('SELECT * FROM contacts') ;
-$cpt = $bdd->query('SELECT COUNT(*) AS nb_contacts FROM contacts') ;
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// En-têtes tableau html : 
+
+echo '<table class="striped"><thead><tr><th>' . 'Id' . '</th><th>' . 'Nom' . '</th><th>' . 'Prénom' . '</th><th>' . 'Entreprise' . '</th><th>' . 'Date de naissance' . '</th><th>' . 'Rue' . '</th><th>' . 'CP' . '</th><th>' . 'Ville' . '</th><th>' . 'Email' . '</th><th>' . 'Tél' . '</th></tr></thead><tbody>'; 
+
+while($donnees=$reponse->fetch()){
+
+	 echo '<tr><td>' . $donnees['id'] . '</td><td>' . $donnees['nom'] . '</td><td>' . $donnees['prenom'] . '</td><td>' . $donnees['entreprise'] . '</td><td>' . $donnees['datenaissance'] . '</td><td>' . $donnees['rue'] . '</td><td>' . $donnees['cp'] . '</td><td>' . $donnees['ville'] . '</td><td>' . $donnees['email'] . '</td><td>' . $donnees['telephone'] . '</td></tr>';	              
+}
+echo '</tbody></table>';
+?>
+
+ <form id="search">
+            <input type="text" name="recherche" id="recherche" value="" placeholder="Rechercher un contact" />
+            <input type="submit" name="search" id="bouton" value="Rechercher">
+        </form>
+
+<div id="codephp">
+
+<!-- $cpt = $bdd->query('SELECT COUNT(*) AS nb_contacts FROM contacts') ;
 $lieu = $bdd->query('SELECT * FROM contacts WHERE ville = "Auch"') ;
 $mail = $bdd->query('SELECT * FROM contacts WHERE email LIKE "%gmail.com"') ;
 
-while($donnees=$reponse->fetch()){
-      echo 'Id= ' . $donnees['id'] . " " . 'Nom= ' . $donnees['nom'] . " " . 'Prénom= ' . $donnees['prenom'] . " " . 'Entreprise= ' . $donnees['entreprise'] . " " . 'Date de naissance= ' . $donnees['datenaissance'] . " " . 'Rue= ' . $donnees['rue'] . " " . 'CP= ' . $donnees['cp'] . " " . 'Ville= ' . $donnees['ville'] . " " . 'Email= ' . $donnees['email'] . " " . 'Tél= ' . $donnees['telephone'] . "\n" . "\n";
-}
+
 while($result=$cpt->fetch()){
       echo 'Nombre de contacts= ' . $result[0] . "\n" . "\n". "\n";
 }
@@ -46,18 +69,56 @@ echo "\n" . "\n";
 while($resmail=$mail->fetch()){
       echo 'Id= ' . $resmail['id'] . " " . 'Nom= ' . $resmail['nom'] . " " . 'Prénom= ' . $resmail['prenom'] . " " . 'Entreprise= ' . $resmail['entreprise'] . " " . 'Date de naissance= ' . $resmail['datenaissance'] . " " . 'Rue= ' . $resmail['rue'] . " " . 'CP= ' . $resmail['cp'] . " " . 'Ville= ' . $resmail['ville'] . " " . 'Email= ' . $resmail['email'] . " " . 'Tél= ' . $resmail['telephone'] . "\n" . "\n";
   }
-echo "\n" . "\n";
+echo "\n" . "\n"; -->
+</div>
 
-$up = $bdd->query('UPDATE contacts SET email = "ogatien@simplon.co" WHERE id = 18') ;
+
+<!-- $up = $bdd->query('UPDATE contacts SET email = "ogatien@simplon.co" WHERE id = 18') ;
 
 // $del = $bdd->query('DELETE FROM appartenir WHERE fk_contact = 5') ;
-// $delcontact = $bdd->query('DELETE FROM contacts WHERE id = 5') ;
+// $delcontact = $bdd->query('DELETE FROM contacts WHERE id = 5') ; -->
 
+<!-- Récupération des données du formulaire d'ajout de contact -->
+<?php 
+
+$req = $bdd->prepare('INSERT INTO contacts (nom, prenom, entreprise, datenaissance, rue, cp, ville, email, telephone) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)');
+
+$req->execute(array($_POST['nom'], $_POST['prenom'], $_POST['entreprise'], $_POST['datenaissance'], $_POST['rue'], $_POST['cp'], $_POST['ville'], $_POST['email'], $_POST['telephone']));
 
  ?>
 
 
 
- 	<footer></footer>
-</body>
+ 
+
+	
+        </body>
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
 </html>
+
+
+  
--- formulaire.php
+++ formulaire.php
@@ -1,14 +1,35 @@
+<?php 
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="form.css">
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title></title>
+	<title>Ajouter un contact</title>
 </head>
 <body>
-	<form method="post" action="annuaire.php"></form>
+	
+	<form method="post" action="annuaire.php">
+		<center><h3>Formulaire de contact</h3></center><br>
+		<h5>Saisir les informations du nouveau contact :</h5><br><br>
+	<div class="formulaire">
+		<label for="nom">Nom : </label><input placeholder="ex : CONNOR" type="text" name="nom" id="nom">
+		<label for="prenom">Prénom : </label><input placeholder="ex : Sarah" type="text" name="prenom" id="prenom">
+		<label for="entreprise">Entreprise : </label><input placeholder="ex : Simplon.co" type="text" name="entreprise" id="entreprise">
+		<label for="datenaissance">Date de naissance : </label><input placeholder="ex : 25/12/1980" type="date" name="datenaissance" id="datenaissance">
+		<label for="rue">Rue : </label><input placeholder="ex : 10 avenue du code" type="text" name="rue" id="rue">
+		<label for="cp">CP : </label><input placeholder="ex : 32000" type="text" name="cp" id="cp">
+		<label for="ville">Ville : </label><input placeholder="ex : Auch" type="text" name="ville" id="ville">
+		<label for="email">E-mail : </label><input placeholder="ex : InCodeWeTrust@simplon.co" type="email" name="email">
+		<label for="telephone">Téléphone : </label><input placeholder="ex : 06 12 34 56 78" name="telephone" type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$">
+		<input type="submit" name="envoi" value="Envoyer" id="envoi">
+	</div>
+	</form>
+	
 </body>
 </html>
+<?php 
+ ?>
